1083fa29e8168bf2d7c8569bc8364144fcbb3cb8
PQDReal <135790194+PQDReal@users.noreply.github.com>
2023-12-20 14:10:06 +0000

    C

--- src/hidden_singles.c
+++ src/hidden_singles.c
@@ -48,7 +48,7 @@ static bool find_hidden_singles_in_unit(Cell **p_cells, int unit_size)
                         {
                             if (p_cells[j]->candidates[k] == p_cells[i]->candidates[c])
                             {
-                                unset_candidate(p_cells[i], p_cells[i]->value);
+                                unset_candidate(p_cells[i], p_cells[i]->candidates[k]);
                                 isHiddenSingle = false;
                                 break;
                             }
