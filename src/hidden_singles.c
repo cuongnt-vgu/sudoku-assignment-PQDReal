fa11be08db6241f7832c35acaa9b29f827813d96
PQDReal <135790194+PQDReal@users.noreply.github.com>
2023-12-21 11:06:23 +0000

    C

--- src/hidden_singles.c
+++ src/hidden_singles.c
@@ -1,11 +1,12 @@
 #include <stddef.h>
 #include "hidden_singles.h"
 #include "sudoku.h"
+#include <string.h>
 
 int hiddenSinglesCounter;
 static int hiddenSinglesCounter_current = 0;
 
-static bool find_hidden_singles_in_unit(SudokuBoard *p_board, Cell **p_cells, int unit_size);
+static bool find_hidden_singles_in_unit(SudokuBoard *p_board, char* text, int unit_size, int i);
 
 // Implement hidden singles logic here
 int hidden_singles(SudokuBoard *p_board)
@@ -15,9 +16,9 @@ int hidden_singles(SudokuBoard *p_board)
         hiddenSinglesCounter = 0;
         for (int i = 0; i < BOARD_SIZE; i++)
         {
-            if (find_hidden_singles_in_unit(p_board, p_board->p_rows[i], BOARD_SIZE)) hiddenSinglesCounter++;
-            if (find_hidden_singles_in_unit(p_board, p_board->p_cols[i], BOARD_SIZE)) hiddenSinglesCounter++;
-            if (find_hidden_singles_in_unit(p_board, p_board->p_boxes[i], BOARD_SIZE)) hiddenSinglesCounter++;
+            if (find_hidden_singles_in_unit(p_board, "row", BOARD_SIZE, i)) hiddenSinglesCounter++;
+            if (find_hidden_singles_in_unit(p_board, "column", BOARD_SIZE, i)) hiddenSinglesCounter++;
+            if (find_hidden_singles_in_unit(p_board, "box", BOARD_SIZE, i)) hiddenSinglesCounter++;
         }
 
         if (hiddenSinglesCounter == 0)
@@ -30,89 +31,262 @@ int hidden_singles(SudokuBoard *p_board)
     return hiddenSinglesCounter_current;
 }
 
-static bool find_hidden_singles_in_unit(SudokuBoard *p_board, Cell **p_cells, int unit_size)
+static bool find_hidden_singles_in_unit(SudokuBoard *p_board, char* text, int unit_size, int i)
 {
-    for (int i = 0; i < unit_size; i++)
+    for (int j = 0; j < unit_size; j++)
     {
-        if (!p_cells[i]->fixed)
+        if (strcmp(text, "row") == 0)
         {
-            for (int c = 0; c < p_cells[i]->num_candidates; c++)
+            if (!p_board->p_rows[i][j]->fixed)
             {
-                bool isHiddenSingle = true;
-
-                for (int j = 0; j < unit_size; j++)
+                for (int c = 0; c < p_board->p_rows[i][j]->num_candidates; c++)
                 {
-                    if (i != j && !p_cells[j]->fixed)
+                    bool isHiddenSingle = true;
+
+                    for (int u = 0; u < unit_size; u++)
                     {
-                        for (int k = 0; k < p_cells[j]->num_candidates; k++)
+                        if (j != u && !p_board->p_rows[i][u]->fixed)
                         {
-                            if (p_cells[j]->candidates[k] == p_cells[i]->candidates[c])
+                            for (int k = 0; k < p_board->p_rows[i][u]->num_candidates; k++)
                             {
-                                isHiddenSingle = false;
-                                break;
+                                if (p_board->p_rows[i][u]->candidates[k] == p_board->p_rows[i][j]->candidates[c])
+                                {
+                                    isHiddenSingle = false;
+                                    break;
+                                }
                             }
                         }
+                    }
+
+                    if (isHiddenSingle)
+                    {
+                        int value = p_board->p_rows[i][j]->candidates[c];
+                        p_board->p_rows[i][j]->value = p_board->p_rows[i][j]->candidates[c];
+                        p_board->p_rows[i][j]->fixed = true;
+                        p_board->p_rows[i][j]->num_candidates = 1;
+                        /*
+                        for (int s = 0; s < p_cells[i]->num_candidates; s++)
+                        {
+                            if (s != c)
+                            {
+                                p_cells[i]->candidates[s] = 0;
+                            }
+                        }
+                        */
+
+                        int r = i;//p_board->p_rows[i][j].row_index;
+                        for (int a = 0; a < unit_size; a++)
+                        {
+                            int num_r = p_board->p_rows[r][a]->num_candidates;
+                            for (int m = 0; m < num_r; m++)
+                            {
+                                if (p_board->p_rows[r][a]->candidates[m] == value)
+                                {
+                                    p_board->p_rows[r][a]->candidates[m] = 0;
+                                    p_board->p_rows[r][a]->num_candidates -= 1;
+                                }
+                            }
+                        }
+
+                        int t = j;
+                        for (int d = 0; d < unit_size; d++)
+                        {
+                            int num_c = p_board->p_rows[t][d]->num_candidates;
+                            for (int n = 0; n < num_c; n++)
+                            {
+                                if (p_board->p_cols[d][t]->candidates[n] == value)
+                                {
+                                    p_board->p_cols[d][t]->candidates[n] = 0;
+                                    p_board->p_cols[d][t]->num_candidates -= 1;
+                                }
+                            }
+                        }
+
+                        int b = (i / 3) * 3 + j / 3;
+                        for (int f = 0; f < unit_size; f++)
+                        {
+                            int num_b = p_board->p_boxes[b][f]->num_candidates;
+                            for (int p = 0; p < num_b; p++)
+                            {
+                                if (p_board->p_boxes[(b / 3) * 3 + f / 3][(b % 3) * 3 + f % 3]->candidates[p] == value)
+                                {
+                                    p_board->p_boxes[(b / 3) * 3 + f / 3][(b % 3) * 3 + f % 3]->candidates[p] = 0;
+                                    p_board->p_boxes[(b / 3) * 3 + f / 3][(b % 3) * 3 + f % 3]->num_candidates -= 1;
+                                }
+                            }
+                        }
+                        return 1;
                     }
                 }
-
-                if (isHiddenSingle)
+            }
+        }
+        else if (strcmp(text, "column") == 0)
+        {
+            if (!p_board->p_cols[j][i]->fixed)
+            {
+                for (int c = 0; c < p_board->p_cols[j][i]->num_candidates; c++)
                 {
-                    int value = p_cells[i]->candidates[c];
-                    p_cells[i]->value = p_cells[i]->candidates[c];
-                    p_cells[i]->fixed = true;
-                    p_cells[i]->num_candidates = 1;
-                    /*
-                    for (int s = 0; s < p_cells[i]->num_candidates; s++)
-                    {
-                        if (s != c)
-                        {
-                            p_cells[i]->candidates[s] = 0;
-                        }
-                    }
-                    */
+                    bool isHiddenSingle = true;
 
-                    int r = p_cells[i]->row_index;
-                    for (int a = 0; a < unit_size; a++)
+                    for (int u = 0; u < unit_size; u++)
                     {
-                        int num_r = p_board->p_rows[r][a]->num_candidates;
-                        for (int m = 0; m < num_r; m++)
+                        if (j != u && !p_board->p_cols[u][i]->fixed)
                         {
-                            if (p_board->p_rows[r][a]->candidates[m] == value)
+                            for (int k = 0; k < p_board->p_cols[u][i]->num_candidates; k++)
                             {
-                                p_board->p_rows[r][a]->candidates[m] = 0;
-                                p_board->p_rows[r][a]->num_candidates -= 1;
+                                if (p_board->p_cols[u][i]->candidates[k] == p_board->p_cols[j][i]->candidates[c])
+                                {
+                                    isHiddenSingle = false;
+                                    break;
+                                }
                             }
                         }
                     }
 
-                    int t = p_cells[i]->col_index;
-                    for (int d = 0; d < unit_size; d++)
+                    if (isHiddenSingle)
                     {
-                        int num_c = p_board->p_rows[t][d]->num_candidates;
-                        for (int n = 0; n < num_c; n++)
+                        int value = p_board->p_cols[j][i]->candidates[c];
+                        p_board->p_cols[j][i]->value = p_board->p_cols[j][i]->candidates[c];
+                        p_board->p_cols[j][i]->fixed = true;
+                        p_board->p_cols[j][i]->num_candidates = 1;
+                        /*
+                        for (int s = 0; s < p_cells[i]->num_candidates; s++)
                         {
-                            if (p_board->p_cols[d][t]->candidates[n] == value)
+                            if (s != c)
                             {
-                                p_board->p_cols[d][t]->candidates[n] = 0;
-                                p_board->p_cols[d][t]->num_candidates -= 1;
+                                p_cells[i]->candidates[s] = 0;
+                            }
+                        }
+                        */
+
+                        int v = i;
+                        for (int a = 0; a < unit_size; a++)
+                        {
+                            int num_r = p_board->p_rows[v][a]->num_candidates;
+                            for (int m = 0; m < num_r; m++)
+                            {
+                                if (p_board->p_rows[v][a]->candidates[m] == value)
+                                {
+                                    p_board->p_rows[v][a]->candidates[m] = 0;
+                                    p_board->p_rows[v][a]->num_candidates -= 1;
+                                }
+                            }
+                        }
+
+                        int t = j;
+                        for (int d = 0; d < unit_size; d++)
+                        {
+                            int num_c = p_board->p_rows[t][d]->num_candidates;
+                            for (int n = 0; n < num_c; n++)
+                            {
+                                if (p_board->p_cols[d][t]->candidates[n] == value)
+                                {
+                                    p_board->p_cols[d][t]->candidates[n] = 0;
+                                    p_board->p_cols[d][t]->num_candidates -= 1;
+                                }
+                            }
+                        }
+
+                        int b = (i / 3) * 3 + j / 3;
+                        for (int f = 0; f < unit_size; f++)
+                        {
+                            int num_b = p_board->p_boxes[b][f]->num_candidates;
+                            for (int p = 0; p < num_b; p++)
+                            {
+                                if (p_board->p_boxes[(b / 3) * 3 + f / 3][(b % 3) * 3 + f % 3]->candidates[p] == value)
+                                {
+                                    p_board->p_boxes[(b / 3) * 3 + f / 3][(b % 3) * 3 + f % 3]->candidates[p] = 0;
+                                    p_board->p_boxes[(b / 3) * 3 + f / 3][(b % 3) * 3 + f % 3]->num_candidates -= 1;
+                                }
+                            }
+                        }
+                        return 1;
+                    }
+                }
+            }
+        }
+        else if (strcmp(text, "box") == 0)
+        {
+            if (!p_board->p_boxes[(i / 3) * 3 + j / 3][(i % 3) * 3 + j % 3]->fixed)
+            {
+                for (int c = 0; c < p_board->p_boxes[(i / 3) * 3 + j / 3][(i % 3) * 3 + j % 3]->num_candidates; c++)
+                {
+                    bool isHiddenSingle = true;
+
+                    for (int u = 0; u < unit_size; u++)
+                    {
+                        if (j != u && !p_board->p_boxes[(i / 3) * 3 + u / 3][(i % 3) * 3 + u % 3]->fixed)
+                        {
+                            for (int k = 0; k < p_board->p_boxes[(i / 3) * 3 + u / 3][(i % 3) * 3 + u % 3]->num_candidates; k++)
+                            {
+                                if (p_board->p_boxes[(i / 3) * 3 + u / 3][(i % 3) * 3 + u % 3]->candidates[k] == p_board->p_boxes[(i / 3) * 3 + u / 3][(i % 3) * 3 + j % 3]->candidates[c])
+                                {
+                                    isHiddenSingle = false;
+                                    break;
+                                }
                             }
                         }
                     }
 
-                    int b = p_cells[i]->box_index;
-                    for (int f = 0; f < unit_size; f++)
+                    if (isHiddenSingle)
                     {
-                        int num_b = p_board->p_boxes[b][f]->num_candidates;
-                        for (int p = 0; p < num_b; p++)
+                        int value = p_board->p_boxes[(i / 3) * 3 + j / 3][(i % 3) * 3 + j % 3]->candidates[c];
+                        p_board->p_boxes[(i / 3) * 3 + j / 3][(i % 3) * 3 + j % 3]->value = value;
+                        p_board->p_boxes[(i / 3) * 3 + j / 3][(i % 3) * 3 + j % 3]->fixed = true;
+                        p_board->p_boxes[(i / 3) * 3 + j / 3][(i % 3) * 3 + j % 3]->num_candidates = 1;
+                        /*
+                        for (int s = 0; s < p_cells[i]->num_candidates; s++)
                         {
-                            if (p_board->p_boxes[(b / 3) * 3 + f / 3][(b % 3) * 3 + f % 3]->candidates[p] == value)
+                            if (s != c)
                             {
-                                p_board->p_boxes[(b / 3) * 3 + f / 3][(b % 3) * 3 + f % 3]->candidates[p] = 0;
-                                p_board->p_boxes[(b / 3) * 3 + f / 3][(b % 3) * 3 + f % 3]->num_candidates -= 1;
+                                p_cells[i]->candidates[s] = 0;
                             }
                         }
+                        */
+
+                        int v = i;
+                        for (int a = 0; a < unit_size; a++)
+                        {
+                            int num_r = p_board->p_rows[v][a]->num_candidates;
+                            for (int m = 0; m < num_r; m++)
+                            {
+                                if (p_board->p_rows[v][a]->candidates[m] == value)
+                                {
+                                    p_board->p_rows[v][a]->candidates[m] = 0;
+                                    p_board->p_rows[v][a]->num_candidates -= 1;
+                                }
+                            }
+                        }
+
+                        int t = j;
+                        for (int d = 0; d < unit_size; d++)
+                        {
+                            int num_c = p_board->p_rows[t][d]->num_candidates;
+                            for (int n = 0; n < num_c; n++)
+                            {
+                                if (p_board->p_cols[d][t]->candidates[n] == value)
+                                {
+                                    p_board->p_cols[d][t]->candidates[n] = 0;
+                                    p_board->p_cols[d][t]->num_candidates -= 1;
+                                }
+                            }
+                        }
+
+                        int b = (i / 3) * 3 + j / 3;
+                        for (int f = 0; f < unit_size; f++)
+                        {
+                            int num_b = p_board->p_boxes[b][f]->num_candidates;
+                            for (int p = 0; p < num_b; p++)
+                            {
+                                if (p_board->p_boxes[(b / 3) * 3 + f / 3][(b % 3) * 3 + f % 3]->candidates[p] == value)
+                                {
+                                    p_board->p_boxes[(b / 3) * 3 + f / 3][(b % 3) * 3 + f % 3]->candidates[p] = 0;
+                                    p_board->p_boxes[(b / 3) * 3 + f / 3][(b % 3) * 3 + f % 3]->num_candidates -= 1;
+                                }
+                            }
+                        }
+                        return 1;
                     }
-                    return 1;
                 }
             }
         }
