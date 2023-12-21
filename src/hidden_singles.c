ebf6b639f99d50a72b70601e2a43d1efdad1e5a6
PQDReal <135790194+PQDReal@users.noreply.github.com>
2023-12-21 09:10:47 +0000

    C

--- src/hidden_singles.c
+++ src/hidden_singles.c
@@ -3,22 +3,25 @@
 #include "sudoku.h"
 
 int hiddenSinglesCounter;
+static int hiddenSinglesCounter_current = 0;
 
-static bool find_hidden_singles_in_unit(Cell **p_cells, int unit_size);
+static bool find_hidden_singles_in_unit(SudokuBoard *p_board, Cell **p_cells, int unit_size);
 
 // Implement hidden singles logic here
 int hidden_singles(SudokuBoard *p_board)
 {
-    hiddenSinglesCounter = 0;
-
     while (1)
     {
+        hiddenSinglesCounter = 0;
         int counter = 0;
         for (int i = 0; i < BOARD_SIZE; i++)
         {
-            if (find_hidden_singles_in_unit(p_board->p_rows[i], BOARD_SIZE)) counter++;
-            if (find_hidden_singles_in_unit(p_board->p_cols[i], BOARD_SIZE)) counter++;
-            if (find_hidden_singles_in_unit(p_board->p_boxes[i], BOARD_SIZE)) counter++;
+            if (find_hidden_singles_in_unit(p_board, p_board->p_rows[i], BOARD_SIZE)) hiddenSinglesCounter++;
+            //pls find a way to remove candidates in the same row, column , box as the hidden single cell
+            if (find_hidden_singles_in_unit(p_board, p_board->p_cols[i], BOARD_SIZE)) hiddenSinglesCounter++;
+            //pls find a way to remove candidates in the same row, column , box as the hidden single cell
+            if (find_hidden_singles_in_unit(p_board, p_board->p_boxes[i], BOARD_SIZE)) hiddenSinglesCounter++;
+            //pls find a way to remove candidates in the same row, column , box as the hidden single cell
         }
 
         if (counter == 0)
@@ -26,11 +29,12 @@ int hidden_singles(SudokuBoard *p_board)
             break;
         }
     }
+    hiddenSinglesCounter_current += hiddenSinglesCounter;
 
     return hiddenSinglesCounter;
 }
 
-static bool find_hidden_singles_in_unit(Cell **p_cells, int unit_size)
+static bool find_hidden_singles_in_unit(SudokuBoard *p_board, Cell **p_cells, int unit_size)
 {
     for (int i = 0; i < unit_size; i++)
     {
@@ -48,7 +52,6 @@ static bool find_hidden_singles_in_unit(Cell **p_cells, int unit_size)
                         {
                             if (p_cells[j]->candidates[k] == p_cells[i]->candidates[c])
                             {
-                                unset_candidate(p_cells[i], p_cells[i]->candidates[k]);
                                 isHiddenSingle = false;
                                 break;
                             }
@@ -58,7 +61,44 @@ static bool find_hidden_singles_in_unit(Cell **p_cells, int unit_size)
 
                 if (isHiddenSingle)
                 {
-                    hiddenSinglesCounter++;
+                    int value = p_cells[i]->candidates[c];
+                    for (int s = 0; s < p_cells[i]->num_candidates; s++)
+                    {
+                        if (s != c)
+                        {
+                            p_cells[i]->candidates[s] = 0;
+                        }
+                    }
+
+                    int r = p_cells[i]->row_index;
+                    for (int a = 0; a < unit_size; a++)
+                    {
+                        for (int m = 0; m < p_board->p_rows[r][a]->num_candidates; m++)
+                        {
+                            if (p_board->p_rows[r][a]->candidates[m] == value)
+                                p_board->p_rows[r][a]->candidates[m] = 0;
+                        }
+                    }
+
+                    int c = p_cells[i]->col_index;
+                    for (int d = 0; d < unit_size; d++)
+                    {
+                        for (int n = 0; n < p_board->p_cols[r][d]->num_candidates; n++)
+                        {
+                            if (p_board->p_cols[c][d]->candidates[n] == value)
+                                p_board->p_cols[c][d]->candidates[n] = 0;
+                        }
+                    }
+
+                    int b = p_cells[i]->box_index;
+                    for (int f = 0; f < unit_size; f++)
+                    {
+                        for (int p = 0; p < p_board->p_boxes[b][f]->num_candidates; p++)
+                        {
+                            if (p_board->p_boxes[b][f]->candidates[p] == value)
+                                p_board->p_boxes[b][f]->candidates[p] = 0;
+                        }
+                    }
                     return true;
                 }
             }
