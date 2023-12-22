e82c84f614c5b12309b1a4423dca0fd14e033773
PQDReal <135790194+PQDReal@users.noreply.github.com>
2023-12-22 19:12:11 +0000

    C

--- src/hidden_singles.c
+++ src/hidden_singles.c
@@ -45,7 +45,7 @@ static int find_hidden_singles_in_unit(SudokuBoard *p_board, int unit_size)
                                 }
                             }
                         }
-                        if (found_r)
+                        if (!found_r)
                         {
                             for (int ad = 0; ad < unit_size; ad++)
                             {
@@ -69,7 +69,7 @@ static int find_hidden_singles_in_unit(SudokuBoard *p_board, int unit_size)
                                     }
                                 }
                             }
-                            if (found_c)
+                            if (!found_c)
                             {
                                 for (int ae = 0; ae < unit_size; ae++)
                                 {
@@ -97,7 +97,7 @@ static int find_hidden_singles_in_unit(SudokuBoard *p_board, int unit_size)
                                     }
                                 }
                             }
-                            if (found_b)
+                            if (!found_b)
                             {
                                 for (int ae = 0; ae < unit_size; ae++)
                                 {
@@ -110,88 +110,6 @@ static int find_hidden_singles_in_unit(SudokuBoard *p_board, int unit_size)
                                 hs_counter++;
                             }
                         }
-                        /*
-                        Cell *r_pos = p_board->p_rows[0][0];
-                        
-                        if (isHiddenSingle_r)
-                        {
-                            r_pos = p_board->p_rows[i][j];
-                            for (int q = 0; q < unit_size; q++)
-                            {
-                                if (q != c)
-                                {
-                                    p_board->p_rows[i][j]->candidates[q] = 0;
-                                }
-                            }
-                            //p_board->p_rows[i][j]->num_candidates = 1;
-                            p_board->p_rows[i][j]->fixed = true;
-                        }
-                        else if (isHiddenSingle_c)
-                        {
-                            r_pos = p_board->p_cols[i][j];
-                            for (int v = 0; v < unit_size; v++)
-                            {
-                                if (v != c)
-                                {
-                                    p_board->p_cols[i][j]->candidates[v] = 0;
-                                }
-                            }
-                            //p_board->p_rows[i][j]->num_candidates = 1;
-                            p_board->p_cols[i][j]->fixed = true;
-                        }
-                        else if (isHiddenSingle_b)
-                        {
-                            r_pos = p_board->p_boxes[i][j];
-                            for (int w = 0; w < unit_size; w++)
-                            {
-                                if (w != c)
-                                {
-                                    p_board->p_boxes[i][j]->candidates[w] = 0;
-                                }
-                            }
-                            //p_board->p_rows[i][j]->num_candidates = 1;
-                            p_board->p_rows[i][j]->fixed = true;
-                        }
-                        if (isHiddenSingle_r || isHiddenSingle_c || isHiddenSingle_b)
-                        {
-                            for (int a = 0; a < unit_size; a++)
-                            {
-                                for (int m = 0; m < unit_size; m++)
-                                {
-                                    if (p_board->p_rows[i][a]->candidates[m] == 1 && m == c && !p_board->p_rows[i][a]->fixed
-                                     && p_board->p_rows[i][a]->row_index != r_pos->row_index &&  p_board->p_rows[i][a]->col_index != r_pos->col_index)
-                                    {
-                                        p_board->p_rows[i][a]->candidates[m] = 0;
-                                        p_board->p_rows[i][a]->num_candidates -= 1;
-                                    }
-                                }
-                            }
-                            for (int b = 0; b < unit_size; b++)
-                            {
-                                for (int n = 0; n < unit_size; n++)
-                                {
-                                    if (p_board->p_cols[i][b]->candidates[n] == 1 && n == c && !p_board->p_cols[i][b]->fixed
-                                     && p_board->p_cols[i][b]->row_index != r_pos->row_index &&  p_board->p_cols[i][b]->col_index != r_pos->col_index)
-                                    {
-                                        p_board->p_cols[i][b]->candidates[n] = 0;
-                                        p_board->p_cols[i][b]->num_candidates -= 1;
-                                    }
-                                }
-                            }
-                            for (int e = 0; e < unit_size; e++)
-                            {
-                                for (int r = 0; r < unit_size; r++)
-                                {
-                                    if (p_board->p_boxes[i][e]->candidates[r] == 1 && r == c && !p_board->p_boxes[i][e]->fixed
-                                     && p_board->p_boxes[i][e]->row_index != r_pos->row_index &&  p_board->p_boxes[i][e]->col_index != r_pos->col_index)
-                                    {
-                                        p_board->p_boxes[i][e]->candidates[r] = 0;
-                                        p_board->p_boxes[i][e]->num_candidates -= 1;
-                                    }
-                                }
-                            }
-                        }
-                        */
                     }
                 }
             }
