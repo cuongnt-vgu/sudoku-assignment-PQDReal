6de0fada65d1c77150640e62d50d3ee7f639451d
PQDReal <135790194+PQDReal@users.noreply.github.com>
2023-12-23 09:41:21 +0000

    C

--- src/hidden_singles.c
+++ src/hidden_singles.c
@@ -1,154 +1,125 @@
-#include <stdlib.h>
 #include "hidden_singles.h"
-#include "sudoku.h"
-#include <string.h>
+#include <stdlib.h>
 
-static int find_hidden_singles_in_unit(SudokuBoard *p_board, int unit_size);
-
-// Implement hidden singles logic here
 int hidden_singles(SudokuBoard *p_board)
 {
-    int hiddenSinglesCounter = 0;
-
-    hiddenSinglesCounter = find_hidden_singles_in_unit(p_board, BOARD_SIZE);
-
-    return hiddenSinglesCounter;
-}
-
-static int find_hidden_singles_in_unit(SudokuBoard *p_board, int unit_size)
-{
-    int hs_counter = 0;
-    int index = 0;
+    int total_cells = 0;
     HiddenSingle *singles = malloc(0);
-    for (int i = 0; i < unit_size; i++)
+
+    // row iteration
+    for (int i = 0; i < BOARD_SIZE; i++) // iterate over every row
     {
-        for (int c = 0; c < unit_size; c++)
+        for (int j = 0; j < BOARD_SIZE; j++) // iterate over every number 1-9
         {
-            int r_count = 0;
-            for (int j = 0; j < unit_size; j++)
+            int candidate_count = 0; // in that row, how many can be j?
+            int single_index = 0; // index of hidden single cell in the row
+            for (int k = 0; k < BOARD_SIZE; k++) // iterate over every cell in the row
             {
-                if (p_board->p_rows[i][j]->candidates[c] == 1)
+                if (p_board->p_rows[i][k]->num_candidates == 1) continue;
+                if (p_board->p_rows[i][k]->candidates[j] == 1)
                 {
-                    index = j;
-                    r_count++;    
+                    candidate_count++;
+                    single_index = k;
                 }
             }
-            if (r_count == 1)
+            if (candidate_count == 1) // only 1 cell can be j
             {
                 HiddenSingle temp_single;
-                temp_single.p_cell = p_board->p_rows[i][index];
-                temp_single.value = c + 1;
+                temp_single.p_cell = p_board->p_rows[i][single_index];
+                temp_single.value = j + 1;
                 int already_checked = 0;
-                for (int l = 0; l < hs_counter; l++)
+                for (int l = 0; l < total_cells; l++)
                     if (singles[l].p_cell == temp_single.p_cell)
                         already_checked = 1;
                 if (!already_checked)
                 {
-                    hs_counter++;
-                    singles = realloc(singles, hs_counter * sizeof(HiddenSingle));
-                    singles[hs_counter - 1] = temp_single;
+                    total_cells++;
+                    singles = realloc(singles, total_cells * sizeof(HiddenSingle));
+                    singles[total_cells - 1] = temp_single;
                 }
             }
         }
     }
-    for (int a = 0; a < unit_size; a++)
+
+    // column iteration
+    for (int i = 0; i < BOARD_SIZE; i++) // iterate over every column
     {
-        for (int d = 0; d < unit_size; d++)
+        for (int j = 0; j < BOARD_SIZE; j++) // iterate over every number 1-9
         {
-            int c_count = 0;
-            for (int b = 0; b < unit_size; b++)
+            int candidate_count = 0;
+            int single_index = 0;
+            for (int k = 0; k < BOARD_SIZE; k++) // iterate over every cell in the column
             {
-                if (p_board->p_cols[a][b]->candidates[d] == 1)
+                if (p_board->p_cols[i][k]->num_candidates == 1) continue;
+                if (p_board->p_cols[i][k]->candidates[j] == 1)
                 {
-                    index = b;
-                    c_count++;
+                    candidate_count++;
+                    single_index = k;
                 }
             }
-            if (c_count == 1)
+            if (candidate_count == 1)
             {
                 HiddenSingle temp_single;
-                temp_single.p_cell = p_board->p_cols[a][index];
-                temp_single.value = d + 1;
+                temp_single.p_cell = p_board->p_cols[i][single_index];
+                temp_single.value = j + 1;
                 int already_checked = 0;
-                for (int l = 0; l < hs_counter; l++)
+                for (int l = 0; l < total_cells; l++)
                     if (singles[l].p_cell == temp_single.p_cell)
                         already_checked = 1;
                 if (!already_checked)
                 {
-                    hs_counter++;
-                    singles = realloc(singles, hs_counter * sizeof(HiddenSingle));
-                    singles[hs_counter - 1] = temp_single;
+                    total_cells++;
+                    singles = realloc(singles, total_cells * sizeof(HiddenSingle));
+                    singles[total_cells - 1] = temp_single;
                 }
             }
         }
     }
-    for (int m = 0; m < unit_size; m++)
+
+    // box iteration
+    for (int i = 0; i < BOARD_SIZE; i++) // iterate over every box
     {
-        for (int q = 0; q < unit_size; q++)
+        for (int j = 0; j < BOARD_SIZE; j++) // iterate over every number 1-9
         {
-            int b_count = 0;
-            for (int n = 0; n < unit_size; n++)
+            int candidate_count = 0;
+            int single_index = 0;
+            for (int k = 0; k < BOARD_SIZE; k++) // iterate over every cell in the box
             {
-                if (p_board->p_rows[m][n]->candidates[q] == 1)
+                if (p_board->p_boxes[i][k]->num_candidates == 1) continue;
+                if (p_board->p_boxes[i][k]->candidates[j] == 1)
                 {
-                    index = n;
-                    b_count++;    
+                    candidate_count++;
+                    single_index = k;
                 }
             }
-            if (b_count == 1)
+            if (candidate_count == 1)
             {
                 HiddenSingle temp_single;
-                temp_single.p_cell = p_board->p_boxes[m][index];
-                temp_single.value = q + 1;
+                temp_single.p_cell = p_board->p_boxes[i][single_index];
+                temp_single.value = j + 1;
                 int already_checked = 0;
-                for (int l = 0; l < hs_counter; l++)
+                for (int l = 0; l < total_cells; l++)
                     if (singles[l].p_cell == temp_single.p_cell)
                         already_checked = 1;
                 if (!already_checked)
                 {
-                    hs_counter++;
-                    singles = realloc(singles, hs_counter * sizeof(HiddenSingle));
-                    singles[hs_counter - 1] = temp_single;
+                    total_cells++;
+                    singles = realloc(singles, total_cells * sizeof(HiddenSingle));
+                    singles[total_cells - 1] = temp_single;
                 }
             }
         }
     }
-/*
-                    for (int w = (i / 3) * 3; w < (i / 3) * 3 + 3; w++)
-                    {
-                        for (int y = (j / 3) * 3; y < (j / 3) * 3 + 3; y++)
-                        {
-                            for (int f = 0; f < unit_size; f++)
-                            {
-                                if (p_board->p_rows[w][y]->fixed == false && p_board->p_rows[w][y]->candidates[f] == 1 && f == c && i != w && j != y)
-                                {
-                                    found_b = true;
-                                }
-                            }
-                        }
-                    }
-                    if (!found_b || !found_c || !found_r)
-                    {
-                        x_pos[hs_counter] = i;
-                        y_pos[hs_counter] = j;
-                        val[hs_counter] = c;
-                        hs_counter++;
-                    }
-                }
-            }
-            }
-        }
-    }
-*/
+
     int *candidates = malloc(4);
-    for (int i = 0; i < hs_counter; i++)
+    for (int i = 0; i < total_cells; i++)
     {
         candidates[0] = singles[i].value;
         set_candidates(singles[i].p_cell, candidates, 1);
     }
     free(singles);
     free(candidates);
-    return hs_counter;
+    return total_cells; // returns total cells solved by hidden singles
 }
 /*
 for (int fd = 0; fd < unit_size; fd++)
