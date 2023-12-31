142622e860559f415a1e36ddedbad98f23b83f5b
PQDReal <135790194+PQDReal@users.noreply.github.com>
2023-12-31 23:22:54 +0700

    Update hidden_singles.h

--- src/hidden_singles.h
+++ src/hidden_singles.h
@@ -2,14 +2,13 @@
 
 #include "sudoku.h"
 
-// Define the HiddenSingle struct
+// Declare functions related to hidden singles
+int hidden_singles(SudokuBoard *p_board)
+
 typedef struct {
     Cell *p_cell;  // Pointer to the cell where the hidden single is found
-    int value;     // The value of the hidden single
+    int value;           // The value of the hidden single
 } HiddenSingle;
-
-// Declare functions related to hidden singles
-int hidden_singles(SudokuBoard *p_board);
 
 
 
