dc7ac20c2874fbc4e6afe6f4b7e8d7435c920b09
PQDReal <135790194+PQDReal@users.noreply.github.com>
2023-12-31 08:39:00 +0000

    Pending changes exported from your codespace

--- src/hidden_pairs.h
+++ src/hidden_pairs.h
@@ -3,7 +3,6 @@
 #include "sudoku.h"
 
 int hidden_pairs(SudokuBoard *p_board);
-int has_same_candidates(const int *candidates1, const int *candidates2);
 
 typedef struct {
     Cell *p_cell_1;  // Pointer to the cell where the hidden single is found
