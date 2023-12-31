22fd14aa5d045d535830b053fe9d80548e6f67f6
PQDReal <135790194+PQDReal@users.noreply.github.com>
2023-12-31 23:12:53 +0700

    Update naked_triples.h

--- src/naked_triples.h
+++ src/naked_triples.h
@@ -2,11 +2,16 @@
 
 #include "sudoku.h"
 
-// Declare functions related to naked pairs
-int naked_triples(SudokuBoard *p_board);
-
-typedef struct {
-    Cell *p_cells[3];
+typedef struct NakedTriples_impl
+{
+    Cell **p_cells;
+    int indices[3];
     int values[3];
-} NakedTriple;
+} NakedTriples;
+
+int is_in_triples(int triples[3], int value);
+int check_cell_in_triples(int triples[3], Cell* p_cell);
+int check_naked_triples(Cell **p_cells, int possible_triples[], int *indices);
+void find_naked_triples(Cell **p_cells, NakedTriples *p_naked_triples, int *p_nt_counter, int unit);
+int naked_triples(SudokuBoard *p_board);
 
