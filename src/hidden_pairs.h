f6f0443fcf788616dc9638e3e3c77bef8d6b280d
PQDReal <135790194+PQDReal@users.noreply.github.com>
2023-12-31 23:32:37 +0700

    Update hidden_pairs.h

--- src/hidden_pairs.h
+++ src/hidden_pairs.h
@@ -11,7 +11,7 @@ typedef struct HiddenPairs_impl
 
 int check_hidden_pairs(Cell **p_cells, int value);
 void find_hidden_pairs_index(Cell** p_cells, int count, int* possible_hidden_values, 
-                        int index, HiddenPairs *p_hidden_pairs, int* p_ht_np_nt_counter);
+                        int index, HiddenPairs *p_hidden_pairs, int* p_counter);
 void find_hidden_pairs(Cell **p_cells, HiddenPairs *p_hidden_pairs,
-                        int *p_ht_np_nt_counter);
+                        int *p_counter);
 int hidden_pairs(SudokuBoard *p_board);
