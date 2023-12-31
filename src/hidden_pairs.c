774be032cd6d5db0d86ad13fbb93dc54282fd87c
PQDReal <135790194+PQDReal@users.noreply.github.com>
2023-12-31 23:32:18 +0700

    Update hidden_pairs.c

--- src/hidden_pairs.c
+++ src/hidden_pairs.c
@@ -13,20 +13,20 @@ int check_hidden_pairs(Cell **p_cells, int value)
     return (count == 2);
 }
 
-void find_hidden_pairs_value(Cell** p_cells, int count, int hidden_value_candidates[], int index, HiddenPairs *hidden_pairs, int* p_ht_np_nt_counter)
+void find_hidden_pairs_value(Cell** p_cells, int count, int possible_hidden_values[], int index, HiddenPairs *p_hidden_pairs, int* p_counter)
 {
     for (int i = 0; i < count; i++)
     {
         for (int j = 0; j < BOARD_SIZE; j++)
         {
             if ((j == index) || (p_cells[j]->num_candidates == 2 && p_cells[index]->num_candidates == 2)) continue;
-            if (is_candidate(p_cells[j], hidden_value_candidates[i]))
+            if (is_candidate(p_cells[j], possible_hidden_values[i]))
             {
                 for (int k = i+1; k < count; k++)
                 {
-                    if (is_candidate(p_cells[j], hidden_value_candidates[k]))
+                    if (is_candidate(p_cells[j], possible_hidden_values[k]))
                     {
-                        hidden_pairs[(*p_ht_np_nt_counter)++] = (HiddenPairs){p_cells, index, {hidden_value_candidates[i], hidden_value_candidates[k]}};
+                        p_hidden_pairs[(*p_counter)++] = (HiddenPairs){p_cells, index, {possible_hidden_values[i], possible_hidden_values[k]}};
                         return;
                     }
                 }
@@ -36,26 +36,26 @@ void find_hidden_pairs_value(Cell** p_cells, int count, int hidden_value_candida
 }
 
 // find hidden pairs in a row, in a collumn or in a box
-void find_hidden_pairs(Cell **p_cells, HiddenPairs *hidden_pairs,
-                        int *p_ht_np_nt_counter)
+void find_hidden_pairs(Cell **p_cells, HiddenPairs *p_hidden_pairs,
+                        int *p_counter)
 {  
     for (int i = 0; i < BOARD_SIZE; i++)
     {
         if (p_cells[i]->num_candidates < 2) continue;
 
         int count = 0;
-        int hidden_value_candidates[BOARD_SIZE];
+        int possible_hidden_values[BOARD_SIZE];
         int* candidates = get_candidates(p_cells[i]);
         for (int j = 0; j < p_cells[i]->num_candidates; j++)
         {
             if (check_hidden_pairs(p_cells, candidates[j]))
             {
-                hidden_value_candidates[count++] = candidates[j];
+                possible_hidden_values[count++] = candidates[j];
             }
         }
         if (count >= 2)
         {
-            find_hidden_pairs_value(p_cells, count, hidden_value_candidates, i, hidden_pairs, p_ht_np_nt_counter);
+            find_hidden_pairs_value(p_cells, count, possible_hidden_values, i, p_hidden_pairs, p_counter);
         }
         free(candidates);
     }
@@ -64,24 +64,25 @@ void find_hidden_pairs(Cell **p_cells, HiddenPairs *hidden_pairs,
 
 int hidden_pairs(SudokuBoard *p_board)
 {
-    int ht_np_nt_counter = 0;
+    int counter = 0;
 
-    HiddenPairs hidden_pairs[BOARD_SIZE * BOARD_SIZE * 9];
+    HiddenPairs p_hidden_pairs[BOARD_SIZE * BOARD_SIZE * 9];
 
     for (int i = 0; i < BOARD_SIZE; i++)
     {
-        find_hidden_pairs(p_board->p_rows[i], hidden_pairs, &ht_np_nt_counter);
-        find_hidden_pairs(p_board->p_cols[i], hidden_pairs, &ht_np_nt_counter);
-        find_hidden_pairs(p_board->p_boxes[i], hidden_pairs, &ht_np_nt_counter);
+        find_hidden_pairs(p_board->p_rows[i], p_hidden_pairs, &counter);
+        find_hidden_pairs(p_board->p_cols[i], p_hidden_pairs, &counter);
+        find_hidden_pairs(p_board->p_boxes[i], p_hidden_pairs, &counter);
     }
 
-    for (int i = 0; i < ht_np_nt_counter; i++)
+    for (int i = 0; i < counter; i++)
     {
-        Cell** p_cells = hidden_pairs[i].p_cells;
-        set_candidates(p_cells[hidden_pairs[i].index], hidden_pairs[i].values, 2);
+        Cell** p_cells = p_hidden_pairs[i].p_cells;
+        set_candidates(p_cells[p_hidden_pairs[i].index], p_hidden_pairs[i].values, 2);
     }
 
-    return ht_np_nt_counter/2;
+    return counter/2;
 }
+
 
 
